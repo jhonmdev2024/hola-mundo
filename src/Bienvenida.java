dc661847023d42c81d5bdb7770c10f838315b5a9
jhonmdev2024 <jhonmdev2024@gmail.com>
2025-12-15 21:31:43 -0500

    feat: modificando Bienvenida, nueva linea de impresion

--- src/Bienvenida.java
+++ src/Bienvenida.java
@@ -1,5 +1,6 @@
 public class Bienvenida {
     public static void main(String[] args) {
         System.out.println("Hola mundo, desde Lima-Peru");
+        System.out.println("Que tal, como estan ?");
     }
 }
